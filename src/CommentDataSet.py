3e64d592bbaabd501655c842d840484cac7071d8
Srirakshith Betageri <sbetageri111@gmail.com>
2019-08-22 10:14:03 -0500

    Return labels as an np array.

--- src/CommentDataSet.py
+++ src/CommentDataSet.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 from torch.utils.data import Dataset
 
 class CommentDataSet(Dataset):
+    COMMENT_COL = 'comment_text'
     def __init__(self, path='../data/train.csv'):
         self.df = pd.read_csv(path)
 
@@ -10,7 +12,12 @@ class CommentDataSet(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-        return self.df.iloc[idx]
+        datapoint = self.df.iloc[idx]
+        comment = datapoint[CommentDataSet.COMMENT_COL]
+        labels = np.array(datapoint[2:].values)
+        labels = labels.astype(int)
+        labels = labels.reshape(labels.shape[0], 1)
+        return comment, labels
 
 
 if __name__ == '__main__':
