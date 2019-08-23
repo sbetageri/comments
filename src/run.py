42078aaf80c5ac0c9788888303ea37a470390b7e
Srirakshith Betageri <sbetageri111@gmail.com>
2019-08-23 13:24:53 -0500

    Save the vocabulary of the corpus.

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import spacy
 import GoodBadDS
+import pickle
 
 from tqdm import tqdm
 
@@ -11,7 +12,9 @@ vocab = set()
 for comment, label in tqdm(ds):
     comment = comment.lower()
     comment = nlp(comment)
-    com_vocab = set(comment)
+    l_doc = [str(token) for token in comment]
+    com_vocab = set(l_doc)
     vocab.update(com_vocab)
 
 print(len(vocab))
+pickle.dump(vocab, open('vocab.set', 'wb'))
