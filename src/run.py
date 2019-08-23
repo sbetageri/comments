ee7d2608a6b8baece23e93758af85ce5d007d754
Srirakshith Betageri <sbetageri111@gmail.com>
2019-08-22 19:55:01 -0500

    Extend vocab check to entire corpus.

--- src/run.py
+++ src/run.py
@@ -8,15 +8,10 @@ ds = GoodBadDS.GoodBadDS()
 nlp = spacy.load('en_core_web_sm')
 
 vocab = set()
-count = 0
 for comment, label in tqdm(ds):
-    count += 1
-    if count > 10:
-        break
     comment = comment.lower()
     comment = nlp(comment)
-    com_vocab = set(comment.text)
+    com_vocab = set(comment)
     vocab.update(com_vocab)
 
-print(vocab)
 print(len(vocab))
