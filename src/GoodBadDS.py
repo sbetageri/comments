e87767b181b8163e3ab7ff4ffab2a00d13872067
Srirakshith Betageri <sbetageri111@gmail.com>
2019-08-24 08:58:39 -0500

    Return 0/1 for good vs bad comments.

--- src/GoodBadDS.py
+++ src/GoodBadDS.py
@@ -1,13 +1,15 @@
+import torch
 from CommentDataSet import CommentDataSet
 
 class GoodBadDS(CommentDataSet):
     def __getitem__(self, idx):
         comment, labels = super(GoodBadDS, self).__getitem__(idx)
 
-        ## comment_type == True implies that the comment is toxic in some form.
+        ## comment_type == 1 implies that the comment is toxic in some form.
         if sum(labels) > 0:
-            comment_type = True
+            comment_type = 1
         else:
-            comment_type = False
+            comment_type = 0
 
+        comment_type = torch.tensor(comment_type)
         return comment, comment_type
